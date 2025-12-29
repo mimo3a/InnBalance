177a8db470e46055ec5e1fe22b7076e8765a3633
Oleksandr <mimo3a@gmail.com>
2025-12-29 12:48:09 +0100

    exercise2

--- InnBalance/src/screens/BreathingScreen.js
+++ InnBalance/src/screens/BreathingScreen.js
@@ -3,24 +3,35 @@ import { View, StyleSheet, TouchableOpacity } from 'react-native';
 import { useRouter } from 'expo-router';
 
 import Exercise1 from '@/src/components/exercise1';
+import Exercise2 from '@/src/components/exercise_2';
 
 export default function BreathingScreen() {
     const router = useRouter();
     return (
-        <View style={styles.container}>
-            <TouchableOpacity style={styles.box} onPress={() => router.push('/exercise')}>
-                <Exercise1 />
-            </TouchableOpacity>
+        <View style={styles.mainContainer}>
+            <View style={styles.container}>
+                <TouchableOpacity style={styles.box} onPress={() => router.push('/exercise')}>
+                    <Exercise1 />
+                </TouchableOpacity>
+            </View>
+    
+            <View style={styles.container}>
+                <TouchableOpacity style={styles.box} onPress={() => router.push('/exercise')}>
+                    <Exercise2 />
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    flexDirection: 'column',
+  },
   container: {
-    flex: 1,  
     margin: 10,
     alignItems: 'center',
-   
   },
   box: {
     margin: 10,
